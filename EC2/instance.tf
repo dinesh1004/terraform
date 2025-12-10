e52490ad0e54928e9ac72e144e58e2ebc316905d
Dinesh velpula <velpuladinesh887@gmail.com>
2025-12-10 16:41:40 +0530

    terraform

--- EC2/instance.tf
+++ EC2/instance.tf
@@ -1,8 +1,28 @@
 resource "aws_instance" "test" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow-tls.id]
 
   tags = {
-    Name = "FIRST"
+    Name = "second"
   }
+}
+
+resource "aws_security_group" "allow-tls" {
+    egress {
+        from_port        = 0 # from port 0 to to port 0 means all ports
+        to_port          = 0
+        protocol         = "-1" # -1 means all protocols
+        cidr_blocks      = ["0.0.0.0/0"] # internet
+  }
+    ingress {
+        from_port        = 0 # from port 0 to to port 0 means all ports
+        to_port          = 0
+        protocol         = "-1" # -1 means all protocols
+        cidr_blocks      = ["0.0.0.0/0"] # internet
+        
+}
+    tags = {
+        Name = "allow-tls"
+    }
 }
