ba47263ef07372ff2fea5d9070f73aab1dacdaa5
Dinesh velpula <velpuladinesh887@gmail.com>
2025-12-18 18:30:17 +0530

    terraform

---
+++ dynamic_block/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.25.0"
+    }
+  }
+   backend "s3" {
+    bucket         = "velpuladinesh-devops-bucket"
+    key            = "Dinesh_test1"
+    region         = "us-east-1"
+    encrypt        = true
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
